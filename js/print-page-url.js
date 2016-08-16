e214d67c2db5734b27471293d5682ed6fe468d19
Scott Gustas <scottius@gmail.com>
2016-08-16 16:48:44 -0400

    Print style edits

--- js/print-page-url.js
+++ js/print-page-url.js
@@ -1,5 +1,5 @@
 (function($, window, document) {
   $(document).ready(function () {
-    $('.td-page-wrap, .post .meta-info').attr('data-post-url', decodeURI(window.location.href));
+    $('.td-content-wrap').attr('data-post-url', decodeURI(window.location.href));
   });
 })(jQuery, window, document);
