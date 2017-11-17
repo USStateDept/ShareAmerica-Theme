eb7acfef4d1329dac58e2542411eef454033afa0
Scott Gustas <scottius@gmail.com>
2017-11-17 11:33:40 -0500

    Add script.js to localize nonce

--- functions.php
+++ functions.php
@@ -25,7 +25,7 @@ function share_add_scripts() {
   // Hide the formidable bottom bar
   wp_enqueue_script( 'formidable-js', get_stylesheet_directory_uri() . '/js/formidable-js.js', array('jquery'), '1.0.0', false );
 
-  wp_enqueue_script( 'share-js', get_stylesheet_directory_uri() . '/js/dist/script.js', array());
+  wp_enqueue_script( 'share-js', get_stylesheet_directory_uri() . '/js/script.js', array());
   // If Timeline plugin is active, load the opacity 0 image fix
   // This fix is necessary because Tagdiv's lazy loading causes photos in the timeline to not display properly.
   // Uses the ready_init: function() in tagdiv_theme.js - if this code is updated, timelinefix.js must be updated.
