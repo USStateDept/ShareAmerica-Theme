b09f21dd17249ce844b3d8bc6f297fe022836cdb
adair76 <priceba@america.gov>
2020-06-30 11:45:24 -0400

    Init frontpage block - clear title and gradient if image post

---
+++ includes/modules/td_module_mx5.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tagdiv
+ * Date: 03.02.2015
+ * Time: 10:05
+ */
+
+class td_module_mx5 extends td_module {
+
+    function __construct($post, $module_atts = array()) {
+        parent::__construct($post, $module_atts);
+    }
+
+    function render($order_no) {
+        ob_start();
+        $title_length = $this->get_shortcode_att('mx5_tl');
+        if ( is_home() || is_front_page() ) {
+            $postID = get_post_format($this->post->ID); // Variable to get Post Format by Type
+            $postFormat = ( ! $postID == "image" || ! $postID == "video" ? "standard" : $postID ); // Variable to determine whether to concat standard or post-format onto the created format class
+        }
+        ?>
+
+        <div class="<?php echo $this->get_module_classes(array("td-big-grid-post-$order_no", "td-big-grid-post", "td-big-thumb", "post-format-$postFormat")); ?>"> <!-- Add class based on post format -->
+            <?php
+                echo $this->get_image('td_534x462');
+            ?>
+            <div class="td-meta-info-container">
+                <div class="td-meta-align">
+                    <div class="td-big-grid-meta">
+                        <?php if (td_util::get_option('tds_category_module_mx5') == 'yes') { echo $this->get_category(); }?>
+                        <?php echo $this->get_title($title_length);?>
+                    </div>
+                    <div class="td-module-meta-info">
+                        <?php echo $this->get_author();?>
+                        <?php echo $this->get_date();?>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <?php return ob_get_clean();
+    }
+}
